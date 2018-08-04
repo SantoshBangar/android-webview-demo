b3480a55c4b0e58162796abae3001fa1aae95d5f
santoshbangar <santubangar7@gmail.com>
2018-08-04 21:22:42 +0530

    - Removed unwanted code.  - Updated the applogo and appname

--- app/src/main/java/com/studentinfo/admin/mattress/MyAppWebViewClient.java
+++ app/src/main/java/com/studentinfo/admin/mattress/MyAppWebViewClient.java
@@ -12,9 +12,9 @@ public class MyAppWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//        return super.shouldOverrideUrlLoading(view, url);
-        if(Uri.parse(url).getHost().endsWith(SplashScreen.ipaddress));
-        return false;
+        return super.shouldOverrideUrlLoading(view, url);
+//        if(Uri.parse(url).getHost().endsWith(SplashScreen.ipaddress));
+//        return false;
 
     }
 //    Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse(url));
